25a5f39946f861e1fcd884204436f73b93a7c232
Dtheran <dtheran1@gmail.com>
2023-02-11 17:22:06 -0500

    ciclos de vida

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,16 +1,49 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   // datos del compon
   // img: string = 'Daniel'
   @Input() img: string = '' // Input representa los props
   @Output() loaded = new EventEmitter<string>()
   imageDefault: string ='../../../assets/pexels-photo-14940646.jpeg'
+
+  constructor() {
+    // before render
+    // No correr cod async
+    // El contructor solo se ejecuta una vez
+    console.log('Constructor', this.img);
+  }
+
+  ngOnChanges() {
+    //Before y durante Render
+    // es un wach de los inputs
+    // se ejecuta cada vez que se actualice un valor del componente
+    console.log('ngOnChanges', this.img);
+  }
+
+  ngOnInit(): void {
+    // before render
+    // aqui si podemos ejecutar codigo async, feching de datos, apis
+    // solo corre una vez once time
+    console.log('Init', this.img);
+  }
+
+  ngAfterViewInit(): void {
+    // corre despues del render
+    // handler children aqui manipulamos a los hijos del compo
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    // sirve para eliminar este componente de la interfaz
+    console.log('ngOnDestroy');
+  }
 
   imgError() {
     this.img = this.imageDefault
